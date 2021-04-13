ef73a6933198778e52988d51989cb1ded81d77bf
NaomiKae <67718740+NaomiKae@users.noreply.github.com>
2021-04-13 02:45:52 -0600

    added css and js from codepen

--- js/main.js
+++ js/main.js
@@ -1,46 +1,68 @@
-var answers = [
-  "It is certain",
-  "It is decidedly so",
-  "Without a doubt",
-  "Yes - definitely",
-  "You may rely on it",
-  "As I see it, yes",
-  "Most likely",
-  "Outlook good",
-  "Yes",
-  "Signs point to yes",
-  "Don't count on it",
-  "My reply is no",
-  "My sources say no",
-  "Outlook not so good",
-  "Very doubtful",
-  "Reply hazy, try again",
-  "Ask again later",
-  "Better not tell you now",
-  "Cannot predict now",
-  "Concentrate and ask again"
-];
-window.onload = function () {
-  var eight = document.getElementById("eight");
+function myFunction() {
+  var popup = document.getElementById("addPopup");
+  popup.classList.toggle("show");
+}
+function siaFunction() {
+  var popup = document.getElementById("addSiaPopup");
+  popup.classList.toggle("show");
+}
+function architectFunction() {
+  var popup = document.getElementById("addArchitectPopup");
+  popup.classList.toggle("show");
+}
+function dukeFunction() {
+  var popup = document.getElementById("addDukePopup");
+  popup.classList.toggle("show");
+}
+function portugalFunction() {
+  var popup = document.getElementById("addPortugalPopup");
+  popup.classList.toggle("show");
+}
 
-  var answer = document.getElementById("answer");
+function portugal2Function() {
+  var popup = document.getElementById("addPortugal2Popup");
+  popup.classList.toggle("show");
+}
 
-  var eightball = document.getElementById("eight-ball");
-  var question = document.getElementById("question");
+function auraFunction() {
+  var popup = document.getElementById("addAuraPopup");
+  popup.classList.toggle("show");
+}
 
-  eightball.addEventListener("click", function () {
-    if (question.value.length < 1) {
-      alert("enter a question!");
-      //if the value in the input form section is left empty, then an alert will pop up.
-    } else {
-      eight.innertext = "";
-      //the inner text that will be displayed equals the statements typed in the quotes above.
-      var num = Math.floor(Math.random() * Math.floor(answers.length));
-      //the number variable equals any whole number out of the total number of possiblities ex. 0-20. To find which number will be displayed, math random function will choose any number from the var answers list that is rounded down to the nearest whole number.
+function bornesFunction() {
+  var popup = document.getElementById("addBornesPopup");
+  popup.classList.toggle("show");
+}
 
-      answer.innerText = answers[num];
-      //inside the "answer" div, the response displayed will equal the number that is randomly picked out of the 20 possibilities of results.
-    }
-  });
-};
-// Thanks to Magic 8 ball Tutorial on SSaurel's Channel
+function weekendFunction() {
+  var popup = document.getElementById("addWeekendPopup");
+  popup.classList.toggle("show");
+}
+
+function madconFunction() {
+  var popup = document.getElementById("addMadconPopup");
+  popup.classList.toggle("show");
+}
+function hozierFunction() {
+  var popup = document.getElementById("addHozierPopup");
+  popup.classList.toggle("show");
+}
+
+function pilotsFunction() {
+  var popup = document.getElementById("addPilotsPopup");
+  popup.classList.toggle("show");
+}
+
+function harryFunction() {
+  var popup = document.getElementById("addHarryPopup");
+  popup.classList.toggle("show");
+}
+function fitzFunction() {
+  var popup = document.getElementById("addFitzPopup");
+  popup.classList.toggle("show");
+}
+
+function bishopFunction() {
+  var popup = document.getElementById("addBishopPopup");
+  popup.classList.toggle("show");
+}
